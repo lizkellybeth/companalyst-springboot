f9dc8debffb03faae6a146ee53ead87c035612f2
Elizabeth Sharp <esharp@nrao.edu>
2021-09-07 15:01:31 -0400

    fixed URL for prod deploy

--- src/main/resources/angular/companalyst-ng/src/app/constants.ts
+++ src/main/resources/angular/companalyst-ng/src/app/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
 
-    public static rootUrl: string = "http://localhost:10001/";
-    //public static rootUrl: string = "https://lizweb-dev.cv.nrao.edu/companalyst/";
+    //public static rootUrl: string = "http://localhost:10001/";
+    public static rootUrl: string = "https://lizweb-dev.cv.nrao.edu/companalyst/";
 }
